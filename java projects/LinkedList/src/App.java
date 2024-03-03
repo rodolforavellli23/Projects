2180170442e23c1515f8c00db403c7da21655e4a
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-03-02 23:52:14 -0300

    Add new project
    Added the linked lists project, other Java stuff broken, I do not understand how to fix it, something to do with the file path and the jdk version.

---
+++ java projects/LinkedList/src/App.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        LinkedList<String> cars = 
+            new LinkedList<String>();
+            cars.add("volvo");
+            cars.add("BMW");
+            cars.add("Ford");
+            cars.add("Mazda");
+            System.out.println("List of cars: " + cars);
+
+    }
+}
--- java projects/demo/src/main/java/com/tutorial/ShowGuests.java
+++ java projects/demo/src/main/java/com/tutorial/ShowGuests.java
@@ -1,25 +1,28 @@
 package com.tutorial;
 
-import static java.lang.System.out;
-import java.util.Scanner;
+import static java.lang.System.*;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
-class ShowGuests {
+public class ShowGuests {
     public static void main(String args[]) throws IOException {
         int guests[] = new int[10];
-        Scanner diskScanner = new Scanner(new File("/home/rodolfo/Documentos/Projetos/java projects/demo/src/Text/guestsList.txt"));
+        Scanner diskScanner = new Scanner(
+                new File("/home/rodolfo/Documentos/Projetos/java projects/demo/src/Text/guestsList.txt"));
 
-        for(int numRoom = 0; numRoom < 10; numRoom++){
+        for (int numRoom = 0; numRoom < 10; numRoom++) {
             guests[numRoom] = diskScanner.nextInt();
         }
 
+        diskScanner.close();
         out.println("Room\tGuests");
 
-        for(int numRoom = 0; numRoom < 10; numRoom++){
+        for (int numRoom = 0; numRoom < 10; numRoom++) {
             out.print(numRoom + 1);
             out.print("\t");
-            out.println(guests[numRoom]);            
+            out.println(guests[numRoom]);
         }
     }
 }
