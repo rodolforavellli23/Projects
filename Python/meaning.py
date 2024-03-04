3192f08cf1e7d5a997254cb6202ddd4e9ef1d890
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-03-03 22:02:55 -0300

    Last change from march 3rd

---
+++ Python/meaning.py
@@ -0,0 +1,21 @@
+# Variables
+
+meaning = 42
+# meaning = 4
+
+# Actions
+
+print('')
+
+# If statement in Python
+
+if meaning > 10:
+    print('Right on brother!')
+else:
+    print('Not today brother!')
+
+# Ternary Operator
+
+# print("Right On!") if meaning >= 10 else print("Not Today!")
+
+print('')
