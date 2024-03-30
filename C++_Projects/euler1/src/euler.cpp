8be0c4aa5a189af948ee89961acfc65c37022bd7
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-03-30 04:32:57 -0300

    New Project Euler Exercise

---
+++ C++_Projects/euler1/src/euler.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <math.h>
+
+/*
+	The sum of all integers that are multiples of 3 & 5, without repetition, that are smaller than 1000, is equal to
+	the sum of all multiples of 3 and 5 up to the element immediately before 1000, minus the multiples of '3 x 5' to
+	avoid double counting.
+*/ 
+
+using namespace std;
+
+int sum3(){
+	int a = ((3+999)*333)/2;
+	return a;
+}
+
+int sum5(){
+	int b = ((5+995)*199)/2;
+	return b;
+}
+
+int sum15(){
+	int c = ((15+990)*66)/2;
+	return c;
+}
+
+int main(){
+
+	int total = 0;
+	total = sum3() + sum5() - sum15();
+
+	cout << "\nThe sum of all integers that are multiples of 3 & 5, without repetition, that are smaller than 1000, is equal to the sum of all multiples of 3 and 5 up to the element immediately before 1000, minus the multiples of '3 x 5' to avoid double counting.";
+	cout << "\n\nThe result is equal to " << total;
+	cout << "\n";
+
+	return 0;
+}
