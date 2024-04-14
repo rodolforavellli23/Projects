9704fb7051d5ad56b6a740231f36390b552089b7
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-04-13 22:14:25 -0300

    created lists file for todays study session

---
+++ Python/lists.py
@@ -0,0 +1,11 @@
+# Study session for April 23, 2024
+
+users = ['Dave','John','Sara'] # List of strings
+
+data = ['Dave', 42, True] # Python allows lists that contain entries of different data types
+
+emptyList = []
+
+print('')
+print('Dave' in emptyList)
+print('')
