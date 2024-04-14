c564514f1c3fa8ac6703aa61ffe8d02100df2156
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-04-13 23:27:12 -0300

    update

--- Python/lists.py
+++ Python/lists.py
@@ -6,6 +6,54 @@ data = ['Dave', 42, True] # Python allows lists that contain entries of differen
 
 emptyList = []
 
-print('')
-print('Dave' in emptyList)
-print('')
+nums = [4,42,78,1,5]
+
+myTuple = tuple(('Dave', 42, True))
+
+anotherTuple = (1,2,3,4,5)
+
+# users.extend(['Elsa','Jason','Robert',"Jimmy"])
+# users.insert(0,'Bob')
+# users[2:2] = ['Eddie','Alex']
+# print('{} \n'.format(users[0:]))
+# print(len(users))
+# print('')
+# users.remove('Bob')
+# print('{} \n'.format(users[0:]))
+# print(users.pop())
+# print('\n{} \n'.format(users[0:]))
+# print(len(users))
+# del users[0]
+# print('\n{} \n'.format(users[0:]))
+# print(len(users))
+# # del data
+# data.clear()
+# print('\n{} \n'.format(data))
+# print(len(data))
+# users.sort()
+# print('\n{} \n'.format(users[0:]))
+# print(len(users))
+
+# print('')
+# print(nums)
+# # nums.reverse() 
+# # # ascending order sorting
+# print('')
+# # print(nums)
+# # print('')
+# # nums.sort(reverse=True)
+# # # Descending order sorting
+# # print(nums)
+# # print('')
+# print(sorted(nums, reverse=True))
+# print('')
+# print(len(nums))
+# print('')
+# print(type(nums))
+# print('')
+
+# Tuples
+
+print(type(myTuple))
+print(type(anotherTuple))
+
