98a8ce91b2ddd06e88c5b74782d27f625ff4b8a0
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-04-19 01:54:12 -0300

    Had to remind myself how c++ strings worked

---
+++ C++_Projects/string test/string_test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string a = "Hello";
+	string b = "World";
+
+	cout << a << " " << b << "!" << endl;
+
+	return 0;
+}
