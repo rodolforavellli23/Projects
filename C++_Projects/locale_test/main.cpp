f29a5482c76ad2ac64c1b1ccd59a36954bb77f64
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-04-19 02:15:38 -0300

    locale
    Had to remind myself how c++ setlocale() function works.

---
+++ C++_Projects/locale_test/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+
+using namespace std;
+
+int main(void)
+{
+  setlocale(LC_ALL, "Portuguese");
+
+  cout << "\n\tUtilizando caracteres e acentuação da língua portuguesa!\n\n" << endl;
+
+  return 0;
+}
