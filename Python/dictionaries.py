cdfcd9f87f82736287f7cbb8c9816df9cb1235e3
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-04-21 13:45:31 -0300

    update(1)

--- Python/dictionaries.py
+++ Python/dictionaries.py
@@ -9,11 +9,25 @@ bands2 = dict(vocals="Plant", guitar="Page")
 
 # print(bands2)
 # print(bands)
-print("There are {} elements".format(len(bands)))
-print(type(bands))
+# print("There are {} elements".format(len(bands)))
+# print(type(bands))
 
 
 # Accessing items inside a dictionary
 
-print(bands["guitar"])
-print(bands.get("vocals"))
+# print(bands["guitar"])
+# print(bands.get("vocals"))
+
+# Listing Keys & Values
+
+# print(bands.keys())
+# print(bands.values())
+
+# Listing a dict as a tuple
+
+# print(bands.items())
+
+# Verify if a key exists
+
+a = "guitar" in bands
+print("{} , guitar in bands = {}".format(bands, a))
