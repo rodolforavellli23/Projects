f07c3d7d9768eb9b6b149bd2c55fc29956cfe1af
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-04-28 03:08:14 -0300

    update

--- Python/loops.py
+++ Python/loops.py
@@ -1,8 +1,23 @@
 # Lesson 8 - Loops
 
-# while loops
+# While loops
 
-var = 0
-while var < 10:
-    print(var)
-    var+=1
+# var = 0
+# while var <= 10:
+#     print("var is equal to {}".format(var))
+#     var+=1
+
+# For loops
+
+# names = ["Dave","Sarah","John"]
+# count = 0
+# for name in names:
+#     print("User number {}, name: {}".format(str(count + 1), name))
+#     count+=1
+
+# for x in "OHIO":
+#     print(x)
+
+for n in range(4):
+    print(n)
+
