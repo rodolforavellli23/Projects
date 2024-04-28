3c30996afa80601cb790e3916a9a5743d0b22316
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-04-28 04:21:23 -0300

    updated rps

--- Python/rps.py
+++ Python/rps.py
@@ -2,31 +2,51 @@
 
 import numpy as np
 
-randint = np.random.randint(1,3)
 
-print('')
-value = input('Enter...\n\nNumber key 1 for Rock, 2 for Paper and 3 for Scissors: ')
-print('')
-print("Computer's choice: {}".format(randint))
-print('')
+repeat = True
 
-playerChoice = int(value)
+while repeat == True:
 
-if playerChoice == randint:
-    print('Tie!')
-elif playerChoice == 1 and randint == 2:
-    print('Rock loses to paper! Computer wins!')
-elif playerChoice == 1 and randint == 3:
-    print('Rock defeats Scissors! Player wins!')
-elif playerChoice == 2 and randint == 3:
-    print('Paper loses to Scissors! Computer wins!')
-elif playerChoice == 2 and randint == 1:
-    print('Paper defeats Rock! Player wins!')
-elif playerChoice == 3 and randint == 1:
-    print('Scissors loses to Rock! Computer wins!')
-elif playerChoice == 3 and randint == 2:
-    print('Scissors defeat Paper! Player wins!')
-else:
-    print('Invalid input!') 
+    randint = np.random.randint(1,3)
 
-print('')
+    print('')
+    value = input('Enter...\n\nNumber key 1 for Rock, 2 for Paper and 3 for Scissors: ')
+    print('')
+    print("Computer's choice: {}".format(randint))
+    print('')
+
+    playerChoice = int(value)
+
+    if playerChoice == randint:
+        print('Tie!')
+    elif playerChoice == 1 and randint == 2:
+        print('Rock loses to paper! Computer wins!')
+    elif playerChoice == 1 and randint == 3:
+        print('Rock defeats Scissors! Player wins!')
+    elif playerChoice == 2 and randint == 3:
+        print('Paper loses to Scissors! Computer wins!')
+    elif playerChoice == 2 and randint == 1:
+        print('Paper defeats Rock! Player wins!')
+    elif playerChoice == 3 and randint == 1:
+        print('Scissors loses to Rock! Computer wins!')
+    elif playerChoice == 3 and randint == 2:
+        print('Scissors defeat Paper! Player wins!')
+    else:
+        print('Invalid input!') 
+
+    print('')
+    
+    playRepeat = input("Play Again? 1 to continue, 0 to exit: ")
+
+    if playRepeat == "0": 
+        print("")
+        break
+    elif playRepeat == "1":
+        print("")
+        continue
+    elif playRepeat != "1" and playRepeat != "0":
+        print("\nInvalid input! Exiting game!\n")
+        repeat = False
+        break
+
+
