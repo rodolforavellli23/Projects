b0df66dd8bbe53186f5abf5084879f936bc484a5
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-04-27 21:37:59 -0300

    grammar correction

--- Python/test.py
+++ Python/test.py
@@ -4,4 +4,4 @@ total_sum = 0
 for i in range(1, 1000):
     if (i % 3 == 0 or i % 5 == 0):
         total_sum = total_sum + i
-print("Euler project's 1st question:\n\'Sum of all integers that are divisible by 3 or by 5 that are greater than 0 and smaller than 1000 is {}\'".format(total_sum))
+print("Euler project's 1st question:\n\'The sum of all integers that are divisible by 3 or by 5 that are greater than 0 and smaller than 1000 is {}\'".format(total_sum))
