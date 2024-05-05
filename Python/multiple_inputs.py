28b98353beee31b803427848de76b6a4d5ef6e8a
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-05-05 02:53:21 -0300

    update

--- Python/multiple_inputs.py
+++ Python/multiple_inputs.py
@@ -4,7 +4,7 @@ def multiple_inputs(*args):
     print("\n{}".format(args))
     print("\n{}".format(type(args)))
 
-# multiple_inputs("Julian",34)
+multiple_inputs("Julian",34)
 # The line above always returns a tuple
 
 def mult_named_inputs(**kwargs):
