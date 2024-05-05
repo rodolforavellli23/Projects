e45190b8ae4162103524773b73eb385d1e37f46e
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-05-05 02:14:16 -0300

    update

--- Python/functions.py
+++ Python/functions.py
@@ -1,10 +1,24 @@
 # Lesson 9 - Python Functions
 
 import getpass
+import subprocess
 
 def hello():
-    print("Hello {}!".format(getpass.getuser().capitalize()))
+    print("\nHello {}!".format(getpass.getuser().capitalize()))
+
+def callUptime():
+    try:
+        subprocess.run(["/home/rodolfo/Documentos/GitHub/Projects/Scripts/firstFunction.sh"], check=True)
+    except subprocess.CalledProcessError as e:
+        subprocess.run(["/home/rodolfo/Documentos/Projetos/Projects/Scripts/firstFunction.sh"])
 
 hello()
 
+value = input("\nPrint uptime? \'y\' \| \'n\': ")
 
+if value != "y" and value != "n":
+    print("\nInvalid Input!\n")
+elif value == "y":
+    callUptime()
+elif value == "n":
+    print("\nOK\n")
