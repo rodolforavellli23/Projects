25885d84143b4210dfa47ad92dd580ca3652075c
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-05-05 02:21:45 -0300

    update

--- Python/functions.py
+++ Python/functions.py
@@ -10,11 +10,12 @@ def callUptime():
     try:
         subprocess.run(["/home/rodolfo/Documentos/GitHub/Projects/Scripts/firstFunction.sh"], check=True)
     except subprocess.CalledProcessError as e:
+        print("\n{}".format(e))
         subprocess.run(["/home/rodolfo/Documentos/Projetos/Projects/Scripts/firstFunction.sh"])
 
 hello()
 
-value = input("\nPrint uptime? \'y\' \| \'n\': ")
+value = input("\nPrint uptime? \'y\' | \'n\': ")
 
 if value != "y" and value != "n":
     print("\nInvalid Input!\n")
@@ -22,3 +23,4 @@ elif value == "y":
     callUptime()
 elif value == "n":
     print("\nOK\n")
+
