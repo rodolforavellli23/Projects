2981af2a250cca6c01a1042ce6505c6bb62a90f1
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-05-05 02:42:11 -0300

    update

---
+++ Python/multiple_inputs.py
@@ -0,0 +1,15 @@
+# Lesson 9 - Python Functions, Exercise 2
+
+def multiple_inputs(*args):
+    print("\n{}".format(args))
+    print("\n{}".format(type(args)))
+
+# multiple_inputs("Julian",34)
+# The line above always returns a tuple
+
+def mult_named_inputs(**kwargs):
+    print("\n{}".format(kwargs))
+    print("\n{}".format(type(kwargs)))
+
+mult_named_inputs(name = "Julian", age = 34)
+# The line above will always return a dict
