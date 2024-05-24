0be303c9c32167318d0c20c8549d41e42011a955
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-05-24 15:21:34 -0300

    created factorial.sh

---
+++ Scripts/factorial.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Factorial function in bash
+
+read -p "Compute the factorial of: " input
+
+input2=$input
+result=1
+
+while [ "$input2" -gt 0 ] ; do
+	result=$[ $result * $input2 ]
+	input2=$[ $input2 - 1 ]
+done
+
+echo "The factorial of $input is $result"
+
