3780ad8d5483bde755fdcc83007e73c0e85befc1
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-05-29 13:35:41 -0300

    updated LaTeX

--- Rust/hello-rust/src/main.rs
+++ Rust/hello-rust/src/main.rs
@@ -12,8 +12,7 @@ fn build() -> String{
 }
 
 fn main() {
-    //Retrieve the rustc version from the variable set by build()
-    
+    //Retrieve the rustc version from the variable set by build()    
     let rustc_version = build();
     println!("\n\tHello World! From rustc version: {}\n", rustc_version);
 }
