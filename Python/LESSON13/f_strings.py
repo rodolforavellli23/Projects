6aec0b3937a8beaaa2bce06a54668e7058a798ba
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-06-09 14:46:01 -0300

    update

--- Python/LESSON13/f_strings.py
+++ Python/LESSON13/f_strings.py
@@ -6,6 +6,17 @@ value = input("\nHow many coins does {} have? ".format(getpass.getuser().capital
 
 player = { 'name': getpass.getuser().capitalize(), 'coins': value} # This is a dictionary
 
-message = "\n{name} has {coins} coins left\n".format(**player) # this value inside the parentheses calls the dictionary
+message = "\n{name} has {coins} coins left.".format(**player) # this value inside the parentheses calls the dictionary
 
 print(message)
+
+##########################################
+# Block 2 - f string formatting
+
+message2 = f"\n{player['name']} has {player['coins']} coins left."
+
+print(message2)
+
+num = float(player['coins'])
+
+print(f"\n2.5 times {num} is roughly equal to {2.5 * num:.2f}\n")
