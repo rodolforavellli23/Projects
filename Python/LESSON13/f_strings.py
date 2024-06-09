7799be7ae2743874fdc60b0e68e50898f0b37e31
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-06-09 14:25:48 -0300

    update

--- Python/LESSON13/f_strings.py
+++ Python/LESSON13/f_strings.py
@@ -1,2 +1,11 @@
 # Lesson 13 - F Strings
 
+import getpass
+
+value = input("\nHow many coins does {} have? ".format(getpass.getuser().capitalize()))
+
+player = { 'name': getpass.getuser().capitalize(), 'coins': value} # This is a dictionary
+
+message = "\n{name} has {coins} coins left\n".format(**player) # this value inside the parentheses calls the dictionary
+
+print(message)
