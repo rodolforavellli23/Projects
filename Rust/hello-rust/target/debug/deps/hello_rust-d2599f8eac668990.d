281ab1ed28089a1b1feddbf3f33841324308361f
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-06-15 22:10:11 -0300

    rust stuff
    probably a mistake

--- Rust/hello-rust/target/debug/deps/hello_rust-d2599f8eac668990.d
+++ Rust/hello-rust/target/debug/deps/hello_rust-d2599f8eac668990.d
@@ -1,5 +1,5 @@
-/home/rodolfo/Documentos/Projetos/Projects/Rust/hello-rust/target/debug/deps/hello_rust-d2599f8eac668990: src/main.rs
+/home/rodolfo/Documentos/GitHub/Projects/Rust/hello-rust/target/debug/deps/hello_rust-d2599f8eac668990: src/main.rs
 
-/home/rodolfo/Documentos/Projetos/Projects/Rust/hello-rust/target/debug/deps/hello_rust-d2599f8eac668990.d: src/main.rs
+/home/rodolfo/Documentos/GitHub/Projects/Rust/hello-rust/target/debug/deps/hello_rust-d2599f8eac668990.d: src/main.rs
 
 src/main.rs:
--- Rust/hello-rust/target/debug/hello-rust.d
+++ Rust/hello-rust/target/debug/hello-rust.d
@@ -1,1 +1,1 @@
-/home/greenshoots/Documentos/GitHub/Projects/Rust/hello-rust/target/debug/hello-rust: /home/greenshoots/Documentos/GitHub/Projects/Rust/hello-rust/src/main.rs
+/home/rodolfo/Documentos/GitHub/Projects/Rust/hello-rust/target/debug/hello-rust: /home/rodolfo/Documentos/GitHub/Projects/Rust/hello-rust/src/main.rs
