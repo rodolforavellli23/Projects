caa8006d8af6812b30ccd61fc3fc6ad497501c4b
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-06-22 13:55:42 -0300

    VB update

---
+++ VB.NET/consoleTest/test/test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace test
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fileVersionInfo.ProductVersion;
+            Console.WriteLine("Hello World! From C# {0}! ", version);
+        }
+    }
+}
---
+++ VB.NET/test/firstAtempt/firstAtempt/MainWindow.cs
@@ -0,0 +1,18 @@
+﻿using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel) => Build();
+
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnButton1Clicked(object sender, System.EventArgs e)
+    {
+        Application.Quit();
+    }
+}
