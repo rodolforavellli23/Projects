51337980facc80607332901c6ced22edb9501473
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-06-27 03:14:33 -0300

    update

--- VB.NET/vscode_helloworld/helloworld/Program.cs
+++ VB.NET/vscode_helloworld/helloworld/Program.cs
@@ -1,10 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.InteropServices;
+
 class Sample
 {
     public static void Main()
     {
-        Console.WriteLine("\nHello World! From .NET {0}\n", Environment.Version.ToString());
+        Console.WriteLine("\nHello World! From {0}\n", RuntimeInformation.FrameworkDescription);
     }
 }
-
