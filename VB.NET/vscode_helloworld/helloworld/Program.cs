20ce167df5776b90d4d4c002f8c262c5004e5b11
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-06-27 03:11:02 -0300

    Update, plus new stuff for C# and .NET

---
+++ VB.NET/vscode_helloworld/helloworld/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Sample
+{
+    public static void Main()
+    {
+        Console.WriteLine("\nHello World! From .NET {0}\n", Environment.Version.ToString());
+    }
+}
+
