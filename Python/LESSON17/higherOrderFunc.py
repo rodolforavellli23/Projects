e7c6d3f5b3402f6098bbc1b7655dc2118af17ade
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-06-29 16:28:20 -0300

    update

--- Python/LESSON17/higherOrderFunc.py
+++ Python/LESSON17/higherOrderFunc.py
@@ -4,17 +4,15 @@ numbers = [1,1,2,3,5,8,13]
 
 squared = map((lambda num : num * num), numbers) 
 
-# 'map()' is a higher order function that returns the output of another function that is passed
-#  as a parameter within itself over a second parameter that can be iterated over 
-#  (ex: lists[], tuples() and dictionaries{}).
+# 'map()' is a higher order function that returns the output of another 
+#  function that is passed as a parameter within itself over a second 
+#  parameter that can be iterated over (ex: lists[], tuples() and dictionaries{}).
 
 repeated = lambda lst : list(dict.fromkeys(lst))
 
-# This lambda function will take the keys from an iterable 
-# and remove all repeated items.
+# This lambda function will take the keys from an iterable and remove all repeated items.
 # 
-# This works by taking a list
-# turning it into a dictionary object(That mantains insertion order)
+# This works by taking a list turning it into a dictionary object( as those mantains insertion order)
 # and casts it back into a list again. 
 
 # -------------------------------------------------------------------
