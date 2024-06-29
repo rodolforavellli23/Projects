b3cace644010544df0ddf05578e16ba33abb47ac
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-06-29 11:48:08 -0300

    update

---
+++ Python/LESSON17/higherOrderFunc.py
@@ -0,0 +1,5 @@
+# Lesson 17 - Higher Order Functions
+
+lambda num: num * num
+
+number = []
--- Python/LESSON17/lambda.py
+++ Python/LESSON17/lambda.py
@@ -2,6 +2,27 @@
 
 squared = lambda num : num * num
 
-value = input("\nType number to be squared: ")
+sumNum = lambda a , b: a + b
 
-print(f"\n{squared(int(value))}\n")
+def funcBuilder(x):
+    return lambda num: num + x
+
+# ===========================================
+
+value1 = input("\nType number to be squared: ")
+# value2 = input("\nType two numbers to be summed: ")
+
+list1 = []
+
+for r in range(2):
+    value2 = input("\nType two numbers to be summed: ")
+    list1.append(int(value2))
+
+print(f"\n{squared(int(value1))}")
+
+print(f"\n{sumNum(list1[0],list1[1])}")
+
+addTen = funcBuilder(10)
+addTwenty = funcBuilder(20)
+
+print(f"\n{addTen(2)}\n\n{addTwenty(2)}\n")
