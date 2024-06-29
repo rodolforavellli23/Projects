6adc644076e325d56b95497c67700ba2a9e7630d
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-06-29 17:00:59 -0300

    update

--- Python/LESSON17/accumulator.py
+++ Python/LESSON17/accumulator.py
@@ -4,6 +4,6 @@ from functools import reduce
 
 numbers = [1,2,3,4,5,1]
 
-total = reduce((lambda acc, curr : acc + curr), numbers)
+total = reduce((lambda acc, curr : acc + curr), numbers, 10)
 
 print(f"\nTotal: {total}\n\nAlso Total, but from sum(): {sum(numbers)}\n")
