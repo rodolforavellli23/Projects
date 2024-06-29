053ee60ed7737c8959c40a9ea88cc6b024a84fd1
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-06-29 12:19:25 -0300

    update

--- Python/LESSON17/higherOrderFunc.py
+++ Python/LESSON17/higherOrderFunc.py
@@ -1,5 +1,10 @@
 # Lesson 17 - Higher Order Functions
 
-lambda num: num * num
+numbers = [1,1,2,3,5,8,13]
 
-number = []
+squared = map((lambda num : num * num), numbers) 
+
+# 'map()' is a higher order function that returns the output of another function that is passed
+#  as a parameter within itself over a second parameter that can be iterated over (ex: lists[], tuples() and dictionaries{})
+
+print(f"\nThe square of the list {numbers} is {list(squared)}\n")
