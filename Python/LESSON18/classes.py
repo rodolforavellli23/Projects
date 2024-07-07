9bc123443d35121d115cf05d237e3d2c8e73e83e
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-07-07 01:01:51 -0300

    Create Lesson 18

---
+++ Python/LESSON18/classes.py
@@ -0,0 +1,42 @@
+# Lesson 18 - Classes in Python
+
+import io # Pyhton module for stream handling.
+import sys # Python built-in functions.
+
+# A class example
+class Vehicle:
+    # Atributes
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    # Methods
+    def moves(self):
+        print("Moves along...")
+
+# An example of a object instance
+my_car = Vehicle("Tesla","Model 3")
+
+# -------------------------------------------------------------------------------------
+
+# This creates the variable that will store the output for the method 'moves'
+captured_output = io.StringIO()
+
+# This will set the stdout to the previously set variable
+sys.stdout = captured_output
+
+# Using a method inherited from the example class
+my_car.moves()
+
+# Reseting stdout to it's original config
+sys.stdout = sys.__stdout__
+
+# Get the captured output as a 'str' value
+var = captured_output.getvalue()
+
+# Close the 'StringIO' object
+captured_output.close()
+
+# -------------------------------------------------------------------------------------
+
+print(f"\nMy car's model: {my_car.model}\n\nMy car's maker: {my_car.make}\n\nIt {var.lower()}")
