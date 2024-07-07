93f23bb510becd4f889b07b0815abb6900dfcb0d
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-07-07 01:53:29 -0300

    update

--- Python/LESSON18/class_inheritance.py
+++ Python/LESSON18/class_inheritance.py
@@ -3,12 +3,18 @@
 from classes import Vehicle
 
 class Airplane(Vehicle):
-    def __init__(self, make, model):
+    def __init__(self, make, model, faa_id):
         super().__init__(make, model)
+        self.faa_id = faa_id
     
+    def set_faa_id(self):
+        self.faa_id = self
+    def get_faa_id(self):
+        return self.faa_id    
+
     def moves(self):
         return "It flies on..."
 
-my_airplane = Airplane("Airbus","A308")
+my_airplane = Airplane("Airbus","A308","N-12345")
 
-print(f"\nMy airplane's maker: {my_airplane.get_make()}\nMy airplane's model: {my_airplane.get_model()}\n{my_airplane.moves()}\n")
+print(f"\nMy airplane's maker: {my_airplane.get_make()}\n\nMy airplane's model: {my_airplane.get_model()}\n\nMy FAA ID: {my_airplane.get_faa_id()}\n\n{my_airplane.moves()}\n")
