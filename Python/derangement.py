e9e7c3b7994465decec83de63c2ee0a6ec1f7ee0
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-07-19 01:15:03 -0300

    update

--- Python/derangement.py
+++ Python/derangement.py
@@ -1,27 +1,38 @@
-# Derangement function
+# Derangement function - Returns how many permutations of a number of 'n' things
+# there are where none of these things are in their original position.
 
-import math
+# import math
 
-def factorial(var):
-    if int(var) == 0 or int(var) == 1:
-        return 1
-    elif int(var) >= 2:
-        return int(var) * factorial( int(var) - 1 )
-    else:
-        return '\nInput not recognized!\n'
+# def factorial(var):
+#     if int(var) == 0 or int(var) == 1:
+#         return 1
+#     elif int(var) >= 2:
+#         return int(var) * factorial( int(var) - 1 )
+#     else:
+#         return '\nInput not recognized!\n'
 
+
+# def derangement(var1):
+#     a = factorial(var1)
+#     b = a / (math.e)
+
+#     if abs(b - (math.floor(b))) < 0.5:
+#         c = (math.floor(b))
+#     else:
+#         c = (math.ceil(b))
+    
+#     return c
 
 def derangement(var1):
-    a = factorial(var1)
-    b = a / (math.e)
-
-    if abs(b - (math.floor(b))) < 0.5:
-        c = (math.floor(b))
-    else:
-        c = (math.ceil(b))
-    
-    return c
-    
-print(f"{derangement(4)}")
+    if int(var1) == 0:
+        return 1
+    elif int(var1) == 1:
+        return 0
+    elif int(var1) >= 2:
+        return (( int(var1) - 1 ) * ((derangement( int(var1) - 1 )) +
+                                     (derangement( int(var1) - 2 ))))
 
 print(f"{derangement(3)}")
+print(f"{derangement(4)}")
+print(f"{derangement(5)}")
+print(f"{derangement(6)}")
