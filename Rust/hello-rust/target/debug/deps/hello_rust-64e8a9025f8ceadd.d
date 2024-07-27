09d335c8ce305ee0902f9ef5e9b9bc53018857b6
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-07-27 18:20:23 -0300

    rust update

---
+++ Rust/hello-rust/target/debug/deps/hello_rust-64e8a9025f8ceadd.d
@@ -0,0 +1,5 @@
+/home/greenshoots/Documentos/GitHub/Projects/Rust/hello-rust/target/debug/deps/hello_rust-64e8a9025f8ceadd: src/main.rs
+
+/home/greenshoots/Documentos/GitHub/Projects/Rust/hello-rust/target/debug/deps/hello_rust-64e8a9025f8ceadd.d: src/main.rs
+
+src/main.rs:
