f5e531756f7ee02161f940f2d2e48f1bddb574c2
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-07-28 19:54:13 -0300

    update

--- Python/LESSON21/weather.py
+++ Python/LESSON21/weather.py
@@ -16,10 +16,11 @@ def get_current_weather():
 
     weather_data = requests.get(request_url).json()
 
-    pprint(weather_data)
+    # pprint(weather_data)
+    print(f'\nCurrent weather for {weather_data["name"]}:')
+    print(f'\nThe temp is {weather_data["main"]["temp"]:.1f}°C')
+    print(
+        f'\n{weather_data["weather"][0]["description"].capitalize()} and feels like {weather_data["main"]["feels_like"]:.1f}°C\n')
 
-# if __name__ == "__main__":
-#     get_current_weather()
-
-get_current_weather()
-
+if __name__ == "__main__":
+    get_current_weather()
