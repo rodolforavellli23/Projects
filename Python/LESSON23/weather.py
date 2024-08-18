8dfba717fdf512b3c36b68c99cab113ee4cd3ef5
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-08-18 11:24:52 -0300

    update

--- Python/LESSON23/weather.py
+++ Python/LESSON23/weather.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()
 
-def get_current_weather(city="Rio de Janeiro"):
+def get_current_weather(city="3451190"):
     
     request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
 
@@ -14,4 +14,16 @@ def get_current_weather(city="Rio de Janeiro"):
     return weather_data
 
 if __name__ == "__main__":
-    get_current_weather()
+
+    print("\n***Get Current Weather Conditions***\n")
+    
+    city = input("Please enter a city name:\n")
+    weather_data = get_current_weather(city)
+
+    print("\n")
+    pprint(weather_data)
+
+    print(f'\nCurrent weather for {weather_data["name"]}:')
+    print(f'\nThe temp is {weather_data["main"]["temp"]:.1f}°C')
+    print(
+        f'\n{weather_data["weather"][0]["description"].capitalize()} and feels like {weather_data["main"]["feels_like"]:.1f}°C\n')
