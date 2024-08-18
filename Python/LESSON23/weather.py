b95671cce22a9d3be4c50706bc8c31c26b57244f
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-08-18 11:07:08 -0300

    final python project

---
+++ Python/LESSON23/weather.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+def get_current_weather(city="Rio de Janeiro"):
+    
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+
+    weather_data = requests.get(request_url).json()
+
+    return weather_data
+
+if __name__ == "__main__":
+    get_current_weather()
