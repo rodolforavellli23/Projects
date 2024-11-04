13903b37d932833b31bb7f6f6b2bd0f20506a2c2
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-11-04 01:10:54 -0300

    new projects

---
+++ C++_Projects/time_project/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	
+	cout << "\n\t" << __DATE__;
+	cout << "\n\n\t" << __TIME__;
+	cout << "\n\n\t" <<__TIMESTAMP__;
+	cout << "\n" << endl;
+	return 0;
+}
---
+++ Scripts/date_custom.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo -ne "\n\tDATA ATUAL: "; date '+%Y || %B || dia %d'; echo -ne "\n"
