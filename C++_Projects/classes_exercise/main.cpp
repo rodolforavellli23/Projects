24b9a7c68505d8af229bdd8d420d24553e53ef98
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2024-12-29 22:22:24 -0300

    updated classes_exersice

--- C++_Projects/classes_exercise/main.cpp
+++ C++_Projects/classes_exercise/main.cpp
@@ -10,16 +10,31 @@ class savingsAccount{
         string name;
 };
 
-int main(int nNumberofArgs, char* pszArgs[]){
+double deposit (savingsAccount& s, double amount){
+    s.balance += amount;
+    return s.balance;
+}
 
+int main(int nNumberofArgs, char* pszArgs[]){
+    // My User
     savingsAccount myAccount;
     myAccount.accNumber = 1234;
     myAccount.balance = 0.0;
     myAccount.name = "Rodolfo";
-
+    int confirm = 0;
+    double amount = 0.0;
+    // Display the user's information
     cout << "\n\tAccount name: " << myAccount.name;
     cout << "\n\tAccount number: " << myAccount.accNumber;
-    // cout << "\tAccount balance: " << myAccount.balance << "\n";
+    printf("\n\tAccount balance: $ %.2f", myAccount.balance);
+    // Make a deposit?
+    cout << "\n\n\tDo a deposit? type 1 for yes, or 0 for no ";
+    cin >> confirm;
+    cout << "\n\tAmount total of deposit: ";
+    cin >> amount;
+    if(confirm){
+        deposit(myAccount, amount);
+    }
     printf("\n\tAccount balance: $ %.2f", myAccount.balance);
     cout << "\n" << endl;
 
