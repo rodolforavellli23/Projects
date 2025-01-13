8bb36d013a0822576e4ae8f8d4f032a5b8b1e7cc
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2025-01-12 21:01:00 -0300

    update

--- C++_Projects/function_overload_intro/Student.h
+++ C++_Projects/function_overload_intro/Student.h
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-
 /*
  * Signing a function as virtual adds polymorphism to that function, which allows
  * for that function to determine to which class the overloaded method belongs
