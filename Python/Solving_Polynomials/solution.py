c2932ce1315b380ca419b5cc786bd51ca680e226
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2025-01-19 22:25:33 -0300

    created an example of how to solve a polynomial using python

---
+++ Python/Solving_Polynomials/solution.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Define the polynomial coefficients in descending order
+coefficients = [24, 8, -975, 0, 8750, 0, -109375]
+
+# Find the roots of the polynomial
+roots = np.roots(coefficients)
+real_roots = [r.real for r in roots if np.isreal(r)]
+
+# for nums in real_roots:
+#     print(f"{float(nums):.4f}")
+
+print(f"\n\tx1 = {float(real_roots[0]):.5f} , x2 = {float(real_roots[1]):.5f}\n")
