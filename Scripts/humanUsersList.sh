1e5c94124ddb05220bd4a90a00a0e4d9a12074d8
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2025-01-29 01:02:00 -0300

    Found online a script for listing all human users. Like w, just dumber.

---
+++ Scripts/humanUsersList.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# List all human users
+all_users=$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd)
+
+# List currently logged-in users
+logged_in_users=$(who | awk '{print $1}' | sort | uniq)
+
+# Display the results
+echo -e "\n\tAll Human Users In This System:"
+echo -e "\t$all_users"
+echo -e "\n\tCurrently Logged-In Human Users:"
+echo -e "\t$logged_in_users\n"
