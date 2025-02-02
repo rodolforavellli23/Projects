42a8101a6c4fcf9f2437e3c8e50e647928b49778
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2025-02-01 23:46:41 -0300

    update

--- lua Projects/numbers_test.lua
+++ lua Projects/numbers_test.lua
@@ -2,4 +2,9 @@ local f = 7.0
 local g = 2.0
 
 print("\n\tf ÷ g =",(f/g))
+
 print("\n\ttype of f:",type(f),"\n","\n\ttype of g:",type(g),"\n")
+
+print(string.format("\t%.7f\n",(math.exp(1))))
+
+print(string.format("\t%.7f\n",(math.pi)))
