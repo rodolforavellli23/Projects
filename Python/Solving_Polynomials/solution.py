7c1861f0f0f5e08dd057a37faa4ae04c1d81fcae
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2025-02-09 00:35:26 -0300

    update

--- Python/Solving_Polynomials/solution.py
+++ Python/Solving_Polynomials/solution.py
@@ -8,7 +8,10 @@ roots = np.roots(coefficients)
 # Create an array with the roots of the polynomial
 real_roots = [r.real for r in roots if np.isreal(r)]
 
-# for nums in real_roots:
-#     print(f"{float(nums):.4f}")
+for nums in real_roots:
+    if(np.sign(nums) == -1):
+        print(f"\t{float(nums)}")
+    elif(np.sign(nums) == 1):
+        print(f"\t+{float(nums)}")
 
 print(f"\n\tx1 = {float(real_roots[0]):.5f} , x2 = {float(real_roots[1]):.5f}\n")
