20e5c83e815d20a0df2c95908736a02cd1369cd1
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2025-02-16 11:57:37 -0300

    update

--- C++_Projects/TemplateVector/main.cpp
+++ C++_Projects/TemplateVector/main.cpp
@@ -8,12 +8,20 @@ using namespace std;
 int main(int argc, char *pArgs[])
 {
     TemplateVector<int> myVector = {10, 20, 30, 40, 50};
+    TemplateVector<string> myVectorString = {"Mathew", "John", "Luke", "Mark"};
 
     cout << "\n\tVector size: " << myVector.size() << " items\n\n";
 
     for (int i = 0; i < myVector.size(); i++)
     {
         cout << "\tItem (" << i << "): " << myVector.get() << endl;
+    }
+
+    cout << "\n\tVector size: " << myVectorString.size() << " items\n\n";
+
+    for (int i = 0; i < myVectorString.size(); i++)
+    {
+        cout << "\tItem (" << i << "): " << myVectorString.get() << endl;
     }
 
     cout << "\n\tPress Enter to continue..." << endl;
