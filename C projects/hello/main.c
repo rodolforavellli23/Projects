67d39016d9e9c6313dc4b574176d2a4bb888d741
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2025-04-27 02:37:29 -0300

    Update main.c

--- C projects/hello/main.c
+++ C projects/hello/main.c
@@ -7,25 +7,26 @@ int main(){
 	char ver[16] = {};
 
 	switch(__STDC_VERSION__){
-	case 199409L:
-		strcat(ver, "C95");
-		break;
-	case 199901L:
-		strcat(ver, "C99");
-		break;
-	case 201112L:
-		strcat(ver, "C11");
-		break;
-	case 201710L:
-		strcat(ver, "C17");
-		break;
-	case 202311L:
-		strcat(ver, "C23");
-		break;
-	default:
-		strcat(ver, "C89/C90");	
-	}
+		case 199409L:
+			strcat(ver, "C95");
+			break;
+		case 199901L:
+			strcat(ver, "C99");
+			break;
+		case 201112L:
+			strcat(ver, "C11");
+			break;
+		case 201710L:
+			strcat(ver, "C17");
+			break;
+		case 202311L:
+			strcat(ver, "C23");
+			break;
+		default:
+			strcat(ver, "C89/C90");	
+		}
 
 	printf("\n\tHello World! From C version: %s \n\n", ver);
+	
 	return 0;
 }
