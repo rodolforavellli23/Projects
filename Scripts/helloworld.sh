0d22e852ef3f7783dd8e3f0a38cd791c4541b5cf
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2025-07-11 00:30:53 -0300

    new update
    learned how to splice strings in bash

--- Scripts/helloworld.sh
+++ Scripts/helloworld.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "Hello World!"
+var=$(bash --version)
+
+sub_var=${var:17:39}
+
+# Splicing strings in BASH works as follows: b=${a:x:y}
+# b is the sub-string, a is the original string,
+# x is the offset (starting point) and 
+# y is the lenght (from how much of the string 
+# beyond the offset must BASH copy to the sub-string.
+
+echo -e "\n\tHello World! From Bash version ${sub_var}\n"
