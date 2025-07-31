98e5192db0095e15ce143742f7a9cbc791e6b1e7
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2025-07-31 02:17:33 -0300

    Create hello.pl

---
+++ Perl_projects/hello/hello.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+use v5.35;
+use English;
+
+# print "\nHello World! From Perl $^V\n\n"
+# The above will print the same as the command below
+
+print "\nHello World! From Perl $PERL_VERSION\n\n";
