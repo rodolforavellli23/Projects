070e21565f19ab45089348d7ad5519b3d648b1cf
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2025-10-19 14:13:26 -0300

    formatting clean up

--- php_projects/helloworld/hello.php
+++ php_projects/helloworld/hello.php
@@ -11,4 +11,5 @@ $sub_str = (substr($ver_str, 0, 9)); # creat a substring with the version value 
 $out = "\n\tHello World! From " . $sub_str . "\n\n"; # Prepare the final string to be printed
 
 echo $out; # Final output of the script
+
 ?>
