d9f874d869c2becd706e4173c7bd69b760b8ae47
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2026-02-04 04:00:54 -0300

    Cont. inverse_3x3_matrix.py
    Completed the second step of 4 to finish this project

--- Python/math_exercises/inverse_3x3_matrix.py
+++ Python/math_exercises/inverse_3x3_matrix.py
@@ -32,18 +32,100 @@ first_line = np.array(list(map(parse_and_check_type, (a.split(", ")))))
 second_line = np.array(list(map(parse_and_check_type, (b.split(", ")))))
 third_line = np.array(list(map(parse_and_check_type, (c.split(", ")))))
 
-input_matrix = np.array([first_line, second_line, third_line])
+np_input_matrix = np.array([first_line, second_line, third_line])
+sp_input_matrix = sp.Matrix(np_input_matrix).applyfunc(sp.nsimplify)
 
-# # Test output for the input matrix
+# # Test output for the Input matrix
 test_prefix = f"{' ':<4}Input = "
-str_out = np.array2string(input_matrix, prefix=test_prefix)
+str_out_1 = np.array2string(np_input_matrix, prefix=test_prefix)
 
-# # Determinant of the input matrix
-sp_input_matrix = sp.Matrix(input_matrix).applyfunc(sp.nsimplify)
+# # Determinant of the Input matrix
 det_input = sp_input_matrix.det()
 
+# # Cofactor matrix
+
+label = f"{' ':<4}Cofactor matrix of the given input matrix: "
+cofactor_prefix = f"{" ":<12}"
+
+# | 00 01 02 |
+# | 10 11 12 |
+# | 20 21 22 |
+
+# -----------------------------------------------------------------
+np_a11 = np.array([[np_input_matrix[1][1], np_input_matrix[1][2]], 
+                   [np_input_matrix[2][1], np_input_matrix[2][2]]])
+
+det_a11 = sp.Matrix(np_a11).applyfunc(sp.nsimplify).det()
+
+# -----------------------------------------------------------------
+np_a12 = np.array([[np_input_matrix[1][0], np_input_matrix[1][2]],
+                   [np_input_matrix[2][0], np_input_matrix[2][2]]])
+
+det_a12 = sp.Matrix(np_a12).applyfunc(sp.nsimplify).det()
+
+# -----------------------------------------------------------------
+np_a13 = np.array([[np_input_matrix[1][0], np_input_matrix[1][1]], 
+                   [np_input_matrix[2][0], np_input_matrix[2][1]]])
+
+det_a13 = sp.Matrix(np_a13).applyfunc(sp.nsimplify).det()
+
+# -----------------------------------------------------------------
+np_a21 = np.array([[np_input_matrix[0][1], np_input_matrix[0][2]], 
+                   [np_input_matrix[2][1], np_input_matrix[2][2]]])
+
+det_a21 = sp.Matrix(np_a21).applyfunc(sp.nsimplify).det()
+
+# -----------------------------------------------------------------
+np_a22 = np.array([[np_input_matrix[0][0], np_input_matrix[0][2]], 
+                   [np_input_matrix[2][0], np_input_matrix[2][2]]])
+
+det_a22 = sp.Matrix(np_a22).applyfunc(sp.nsimplify).det()
+
+# -----------------------------------------------------------------
+np_a23 = np.array([[np_input_matrix[0][0], np_input_matrix[0][1]], 
+                   [np_input_matrix[2][0], np_input_matrix[2][1]]])
+
+det_a23 = sp.Matrix(np_a23).applyfunc(sp.nsimplify).det()
+
+# -----------------------------------------------------------------
+np_a31 = np.array([[np_input_matrix[0][1], np_input_matrix[0][2]], 
+                   [np_input_matrix[1][1], np_input_matrix[1][2]]])
+
+det_a31 = sp.Matrix(np_a31).applyfunc(sp.nsimplify).det()
+
+# -----------------------------------------------------------------
+np_a32 = np.array([[np_input_matrix[0][0], np_input_matrix[0][2]], 
+                   [np_input_matrix[1][0], np_input_matrix[1][2]]])
+
+det_a32 = sp.Matrix(np_a32).applyfunc(sp.nsimplify).det()
+
+# -----------------------------------------------------------------
+np_a33 = np.array([[np_input_matrix[0][0], np_input_matrix[0][1]],
+                   [np_input_matrix[1][0], np_input_matrix[1][1]]])
+
+det_a33 = sp.Matrix(np_a33).applyfunc(sp.nsimplify).det()
+
+# -----------------------------------------------------------------
+sp_cofactor_matrix = sp.Matrix([[ det_a11, -det_a12,  det_a13],
+                                [-det_a21,  det_a22, -det_a23],
+                                [ det_a31, -det_a32,  det_a33]])
+
+cofactor_line_1 = np.array(list(map(parse_and_check_type, sp_cofactor_matrix[0, :])))
+cofactor_line_2 = np.array(list(map(parse_and_check_type, sp_cofactor_matrix[1, :])))
+cofactor_line_3 = np.array(list(map(parse_and_check_type, sp_cofactor_matrix[2, :])))
+
+np_cofactor_matrix = np.array([cofactor_line_1, cofactor_line_2, cofactor_line_3])
+
+str_out_2 = np.array2string(np_cofactor_matrix, prefix=cofactor_prefix,
+                                                formatter={'float_kind': lambda x: f"{x: >6.1f}", # ' >' forces alignment and sign space
+                                                           'int_kind': lambda x: f"{x: >4d}"})
+
+# # TODO: Create and print the Transpose matrix of the Cofactor matrix
+
 # Text Output
-print(f"\n{test_prefix}{str_out}")
+print(f"\n{test_prefix}{str_out_1}")
 print(f"\n{' ':<4}{'':{'-'}<80}\n")
 print(f"{' ':<4}Determinant of the input matrix: {det_input}")
+print(f"\n{' ':<4}{'':{'-'}<80}\n")
+print(f"{label}\n\n{cofactor_prefix}{str_out_2}")
 print(f"\n{' ':<4}{'':{'-'}<80}\n")
--- Python/math_exercises/matrix_mult.py
+++ Python/math_exercises/matrix_mult.py
@@ -4,30 +4,38 @@
 
 # Imported libraries
 import numpy as np
+import sympy as sp
 
 # Variable declaration
+
 prefix_1 = f"{' ':<4}Matrix A = "
 prefix_2 = f"{' ':<4}Matrix A^-1 = " 
 prefix_3 = f"{' ':<4}A*A^-1 = I; I = "
 
-matrix_A = np.array([[1, 2, 3],
-                     [0, 1, 4],
-                     [5, 6, 0]])
+np_matrix_A = np.array([[1, 2, 3],
+                        [0, 1, 4],
+                        [5, 6, 0]])
 
-matrix_A_inver = np.array([[-24,  18,  5],
-                           [ 20, -15, -4],
-                           [ -5,   4,  1]])
+# Calculating the Inverse matrix
 
-# Matrix multiplication
-matrix_I = np.matmul(matrix_A, matrix_A_inver)
+sp_matrix_A = sp.Matrix(np_matrix_A)
+
+sp_matrix_A_inver = sp.Matrix.inv(sp_matrix_A) 
+
+np_matrix_A_inver = np.array(sp_matrix_A_inver).astype(np.int64)
+
+# Calculating the Identity matrix
+# Have to use sympy here for exact results
+
+sp_matrix_I = sp_matrix_A * sp_matrix_A_inver
+np_matrix_I = np.array(sp_matrix_I).astype(np.int64)
 
 # String formatting
-str_1 = np.array2string(matrix_A, prefix=prefix_1)
-str_2 = np.array2string(matrix_A_inver, prefix=prefix_2)
-str_3 = np.array2string(matrix_I, prefix=prefix_3)
+str_1 = np.array2string(np_matrix_A, prefix=prefix_1)
+str_2 = np.array2string(np_matrix_A_inver, prefix=prefix_2, formatter={'int': lambda x: f"{x:3d}"})
+str_3 = np.array2string(np_matrix_I, prefix=prefix_3, formatter={'int': lambda x: f"{x:1d}"})
 
 # Text output
 print(f"\n{prefix_1}{str_1}\n")
 print(f"{prefix_2}{str_2}\n")
 print(f"{prefix_3}{str_3}\n")
-
