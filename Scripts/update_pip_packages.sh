b5cbf7d4de2b055f133e89ba5a4fa21fc6839cce
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2026-05-18 01:02:35 -0300

    Update update_pip_packages.sh

--- Scripts/update_pip_packages.sh
+++ Scripts/update_pip_packages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 # Small script to just update pip packages inside a virtual environment
-pip list --format=freeze | cut -d= -f1 | xargs pip install -U
+pip list --format=freeze | cut -d "=" -f 1 | xargs pip install -U
