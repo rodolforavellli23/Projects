1a4d6ab174fe7fede5a5cb6dd0eed38d9d66b058
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2026-06-29 00:43:46 -0300

    Create linear_interpolation.py

---
+++ Python/math_exercises/linear_interpolation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# script to solve simple linear interpolations
+
+"""
+
+slope: m = (y2 - y1)/(x2 - x1)
+
+linear function: (y(x) - y1) = (x - x1)*m
+
+"""
+
+import numpy as np
+
+# Known data points
+x_data_Biot = np.array([0.8, 0.9])
+y1_data_A1 = np.array([1.2236, 1.2488])
+y2_data_lambda1 = np.array([1.4320, 1.5044])
+
+# New point to interpolate to
+x_new = 0.82
+
+# Interpolatet value
+y_A1_new = np.interp(x_new, x_data_Biot, y1_data_A1)
+y_lambda1_new = np.interp(x_new, x_data_Biot, y2_data_lambda1)
+
+# Output
+print(f"\n{' ':<4}For Biot = 0.82; the sphere values for A1 and lambda1 are:\n"
+      f"\n{' ':<4}A1 = {y_A1_new:.4f}"
+      f"\n{' ':<4}lambda1 = {y_lambda1_new:.4f}"
+      f"\n")
