3b4fda2d125471466540d582239b0364b6b53d40
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2026-07-23 23:24:08 -0300

    Update hello.exs

--- elixir/hello-world/hello.exs
+++ elixir/hello-world/hello.exs
@@ -4,6 +4,6 @@ pad = "    "
 
 {cons_input, 0} = System.cmd("elixir", ["--version"])
 
-vers = String.trim(String.slice(cons_input, 100..144))
+vers = String.trim(String.slice(cons_input, 101..145))
 
 IO.puts("\n#{pad}Hello World! From #{vers}\n")
