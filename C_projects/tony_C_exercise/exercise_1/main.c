7329268fd188b3d2fb3d9446b8d5cd5353ad98a9
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2026-07-31 23:23:01 -0300

    update 1
    Working on C tutorial
    
    Making a backup of my gtk.css file for xfce

--- C_projects/tony_C_exercise/exercise_1/main.c
+++ C_projects/tony_C_exercise/exercise_1/main.c
@@ -5,11 +5,18 @@
 
 int main(int argc, char *argv[]) {
 	// Start
+
 	// Variables
-	const char *pad = "    ";
+	//
+	// Formatted pad string
+	char pad[8];
+	size_t pad_t = sizeof(pad)/sizeof(pad[0]);
+	int pad_count = snprintf(pad, pad_t, "%4s", " ");
+	
+	// Path
 	const char *path = (argc > 1)? argv[1] : ".";
 
-	// Text 
+	// Text Output 
 	printf("\n%s%s\n\n", pad, path);
 
 	// End
