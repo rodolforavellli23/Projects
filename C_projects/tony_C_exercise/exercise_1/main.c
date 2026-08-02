1cdb26352d62253a5300c4e8e482b62034503385
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2026-08-02 01:38:57 -0300

    update

--- C_projects/tony_C_exercise/exercise_1/main.c
+++ C_projects/tony_C_exercise/exercise_1/main.c
@@ -4,21 +4,59 @@
 #include<dirent.h>
 #include<errno.h>
 #include<string.h>
+#include<unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 // tony C tutorial - How ls works?
 
+// Global Variables
+int show_all = 0;
+
+// Functions
+void print_usage_stdout(const char *prog_name, const char *pad) {
+	fprintf(stdout, "\n%sTony's ls clone tutorial\n", pad);
+	fprintf(stdout, "\n%sUsage: %s [ -h , -a ] [path]\n", pad, prog_name);
+	fprintf(stdout, "%s%s-h\tPrints this help messagen\n", pad, pad);
+	fprintf(stdout, "%s%s-a\tShows hidden files and directories\n", pad, pad);
+	fprintf(stdout, "\n");
+}
+
+void print_usage_stderr(const char *prog_name, const char *pad) {
+	fprintf(stderr, "\n%sTony's ls clone tutorial\n", pad);
+	fprintf(stdout, "\n%sUsage: %s [ -h , -a ] [path]\n", pad, prog_name);
+	fprintf(stdout, "%s%s-h\tPrints this help messagen\n", pad, pad);
+	fprintf(stdout, "%s%s-a\tShows hidden files and directories\n", pad, pad);
+	fprintf(stdout, "\n");
+}
+
+// Program Entry Point
 int main(int argc, char *argv[]) {
 	// Start
 
-	// Variables
-	//
 	// Formatted pad string
 	char pad[8];
 	size_t pad_t = sizeof(pad)/sizeof(pad[0]);
 	int pad_count = snprintf(pad, pad_t, "%4s", " ");
-	
+
+	// Options
+	int opt;
+	while((opt = getopt(argc, argv, "ah")) != -1) {
+		switch(opt) {
+			case 'a':
+				show_all = 1;
+				break;
+			case 'h':
+				print_usage_stdout(argv[0], pad);
+				return 0;
+			default:
+				print_usage_stderr(argv[0], pad);
+				return 1;
+		}
+	}
+
 	// Path
-	const char *path = (argc > 1)? argv[1] : ".";
+	const char *path = (optind < argc)? argv[optind] : ".";
 
 	// Directory entity
 	DIR *dir = opendir(path);
@@ -28,8 +66,8 @@ int main(int argc, char *argv[]) {
 	entry = readdir(dir);
 
 	// Error handling
-	//
-	// opendir error
+
+	/* opendir error */
 	char error_mesg[16];
 	size_t emes_t = sizeof(error_mesg)/sizeof(error_mesg[0]);
 	int emes_count = snprintf(error_mesg, emes_t, "\n%sopendir", pad);
@@ -39,9 +77,9 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 	
-	// Text Output -- To Do: Replace this logic with getopt
+	// Text Output
 	printf("\n");
-	if (argv[2] && (strncmp(argv[2], "-a", 2)) == 0 ) {
+	if (show_all == 1 ) {
 		while ((entry = readdir(dir)) != NULL) {
 			printf("%s%s\n", pad, entry->d_name);
 		}
