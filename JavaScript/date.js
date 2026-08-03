1555dba3084490367e2a0c17d7188cfcf5a1621a
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2026-08-03 03:26:18 -0300

    update

--- JavaScript/date.js
+++ JavaScript/date.js
@@ -1,3 +1,13 @@
 #!/usr/bin/node
 
-console.log(`\n\t${new Date().toString()}`)
+function padding(size) {
+	let pad = "";
+	for(let i = 0; i < size; i++) {
+		pad+=" ";
+	}
+	return pad
+}
+
+var my_pad = padding(4);
+
+console.log(`\n${my_pad}${new Date().toString()}\n`)
--- JavaScript/hello_node.js
+++ JavaScript/hello_node.js
@@ -1,3 +1,29 @@
 #!/usr/bin/node
 
-console.log(`\n\tHello World! From Node ${process.version} at ${process.platform}!`)
+// Notes:
+//
+// JavaScript slices are 'end exclusive'
+
+// Functions
+function padding(size) {
+	let pad = "";
+	for(let i = 0; i < size; i++) {
+		pad+=" ";
+	}
+	return pad;
+}
+
+function captalize_first_char(my_string) {
+	let first_char  = my_string.slice(0, 1);
+	let rest_string = my_string.slice(1);
+
+	let result = first_char.toUpperCase() + rest_string;
+	return result;
+}
+
+// Variables
+var my_pad = padding(4);
+
+// Text Output
+console.log(`\n${my_pad}Hello World! From Node ${process.version} at ` +
+	    `${captalize_first_char(process.platform)}!\n`);
