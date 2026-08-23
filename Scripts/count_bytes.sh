634f6f29d36bd87630b2f07b7ce26328837d0689
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2026-08-23 05:24:27 -0300

    Update count_bytes.sh

--- Scripts/count_bytes.sh
+++ Scripts/count_bytes.sh
@@ -1,28 +1,43 @@
 #!/usr/bin/bash
 
-# Print the name of the given program/file and 
+# Print the name of the given program/file and
 # the number of bytes it occupies in memory
 
-# Color definitions - alternative scheme
+# Definitions: pad, colors, usage message
 
-declare -g NAME_COLOR='\033[1;32m'       # Bright green for the program name
-declare -g SIZE_COLOR='\033[1;38;5;208m' # Bright orange for the program size
-declare -g NO_COLOR='\033[0m'            # No Color
+# Decorative blank pad
+myPad=$(printf "%4s" " ")
+
+# Bright green for the program name
+NAME_COLOR='\033[1;32m'
+
+# Bright orange for the program size
+SIZE_COLOR='\033[1;38;5;208m'
+
+# Bright red for thr error color
+ERROR_COLOR='\033[38;2;255;102;102m'
+
+# No Color
+NO_COLOR='\033[0m'
+
+# Usage instructions
+usage="\n${myPad}${NAME_COLOR}Usage:${NO_COLOR}"
+usage+="\n${myPad}<This Script> <Program Name>\n"
 
 # Function definition
 countBytes() {
 	# Array with sufixes
 	declare -a t_bytes=( \
-		"B"          \
-		"KB"         \
-		"MB"         \
-		"GB"         \
+		"B"              \
+		"KB"             \
+		"MB"             \
+		"GB"             \
 	)
 	# Index to put in the array
 	declare -i Count=$(awk "BEGIN {
 		i = $1;
 		c = 0;
-		while (i >= 1024 && c <= 3) {
+		while (i >= 1024 && c <= ${#t_bytes[@]}) {
 			i /= 1024;
 			c++;
 		}
@@ -40,17 +55,29 @@ countBytes() {
 	echo "${Size} ${t_bytes[${Count}]}"
 }
 
+# Error Checking
+if [[ "$#" -eq 0 ]]; then
+	error1="\n${myPad}${ERROR_COLOR}Error: ${NO_COLOR}No argument has been passed"
+	echo -e "$error1\n$usage"
+	exit 1
+elif [[ "$#" -gt 1 ]]; then
+	error2="\n${myPad}${ERROR_COLOR}Error: ${NO_COLOR}This script may only accept one argument"
+	echo -e "$error2\n$usage"
+	exit 1
+elif ! ( which "$1" ) &> /dev/null; then
+	error3="\n${myPad}${ERROR_COLOR}Error: ${NO_COLOR}No program with this name is installed"
+	echo -e "$error3\n$usage"
+	exit 1
+fi
+
+# Note: 2> /dev/null will redirect any error messages to /dev/null, effectively
+# suppressing them. &> /dev/null will suppress both the stdout and stderr streams.
+
 # Variable definitions
-declare -g myPad=$(printf "%4s" " ")
-declare -g mySize=$(wc -c < $(which "$1"))
-declare -g myResult="$(countBytes "$mySize")"
+mySize=$(wc -c < $(which "$1"))
+myResult="$(countBytes "$mySize")"
 
 # Script Output
-printf "\n%sSize of %b%s%b in disk storage: %b%s%b\n\n" \
-	"$myPad"                                        \
-	"$NAME_COLOR"                                   \
-	"$1"                                            \
-	"$NO_COLOR"                                     \
-	"$SIZE_COLOR"                                   \
-	"$myResult"                                     \
-	"$NO_COLOR"
+myOutput="\n${myPad}Size of ${NAME_COLOR}${1}${NO_COLOR} in disk storage: ${SIZE_COLOR}${myResult}${NO_COLOR}\n"
+
+echo -e "$myOutput"
