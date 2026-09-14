3475d12c12431302d3f673e3e5c3b9d81485c84b
rodolforavellli23 <38845400+rodolforavellli23@users.noreply.github.com>
2026-09-13 21:39:46 -0300

    Update test_main.c

--- C_projects/hello/test_main.c
+++ C_projects/hello/test_main.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char* pad(int size, char c) {
+
+	char *result = malloc(size * sizeof(char));
+
+	// malloc error checking
+	if (result == NULL) {
+		return NULL;
+	}
+
+	// Append to result using a loop
+	for (int i = 0; i < size; i++) {
+		result[i] = c;
+	}
+
+	// Make sure the result string is null terminated
+	result[size] = '\0';
+
+	return result;
+}
 
 char* version(long int c) {
-	
+
 	char* ver_string;
-	
+
 	switch(c) {
 		case 199409L:
 			ver_string = "C95";
@@ -25,16 +46,35 @@ char* version(long int c) {
 			ver_string = "C89/C90 (or non-compliant)";
 			break;
 	}
-	
+
 	return ver_string;
 }
 
 int main(void) {
-  
-    printf("\n%4cHello World! From C version: %s\n\n", ' ', version(__STDC_VERSION__));
-    
-    //This Checks what number the macro is actually returning to the program
-    //printf("%4c%ld\n", ' ', __STDC_VERSION__);
+	// Variables
+	char* message   = "Hello World! From C version: ";
+	char* pad_star  = pad(5, '*');
+	char* pad_space = pad(1, ' ');
+	char* newl_pad  = pad(4, ' ');
 
-    return 0;
+	char start_pad[8], end_pad[8], f_message[64];
+
+	(void)snprintf(start_pad   , sizeof(start_pad), "%s%s", pad_star , pad_space);
+	(void)snprintf(end_pad     , sizeof(end_pad)  , "%s%s", pad_space, pad_star );
+	(void)snprintf(f_message   , sizeof(f_message), \
+			"\n%s%s%s%s%s\n\n",                     \
+			newl_pad,                               \
+			start_pad,                              \
+			message,                                \
+			version(__STDC_VERSION__),              \
+			end_pad);
+
+	// Text Output
+	printf("%s", f_message);
+
+	// End of Program
+	free(pad_star);
+	free(pad_space);
+	free(newl_pad);
+	return 0;
 }
